4c882d4817b8ac74a48095f4f9aafa3a4b7f1f9c
Orrero-yang <orreroyang@gmail.com>
2022-12-05 10:09:47 +0800

    20221205

---
+++ Luogu/101/P2433.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    if (T == 1) {
+        cout << "I love Luogu!";
+    } else if (T == 2) {
+        cout << 2 + 4 << " " << 10 - 2 - 4;
+    } else if (T == 3) {
+        cout << ceil(14/4) << endl;
+        cout << 4 * ceil(14/4) << endl;
+        cout << 14 - ceil(14/4) * 4 << endl;
+    } else if (T == 4) {
+        // 请自行完成问题 4 的代码
+    } else if (T == 5) {
+        // 请自行完成问题 5 的代码
+    } else if (T == 6) {
+        // 请自行完成问题 6 的代码
+    } else if (T == 7) {
+        // 请自行完成问题 7 的代码
+    } else if (T == 8) {
+        // 请自行完成问题 8 的代码
+    } else if (T == 9) {
+        // 请自行完成问题 9 的代码
+    } else if (T == 10) {
+        // 请自行完成问题 10 的代码
+    } else if (T == 11) {
+        // 请自行完成问题 11 的代码
+    } else if (T == 12) {
+        // 请自行完成问题 12 的代码
+    } else if (T == 13) {
+        // 请自行完成问题 13 的代码
+    } else if (T == 14) {
+        // 请自行完成问题 14 的代码
+    }
+    return 0;
+}
