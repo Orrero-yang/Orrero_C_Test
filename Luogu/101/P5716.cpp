eb20e95dc1244bd72536903f6ce68cdd82a09dd5
Orrero-yang <orreroyang@gmail.com>
2022-12-05 15:07:18 +0800

    20221205

--- Luogu/101/P5716.cpp
+++ Luogu/101/P5716.cpp
@@ -1,8 +1,12 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int y, m;
-int main(){
-    cin >> y >> m;
-    
+int year,month;
+int a[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+int main()
+{
+    cin>>year>>month;
+    if((year%4==0&&year%100!=0)||year%400==0)a[2]=29;
+    cout<<a[month];
+    return 0;
 }
